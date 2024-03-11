405cefea3a49471c3935a78c09b88fe3f4b6974d
Enzo Brincalepe <enzo.brincalepe@gmail.com>
2024-03-11 00:43:43 -0300

    landing page

---
+++ script.js
@@ -0,0 +1,20 @@
+let aboutMePrincipal = document.getElementsByClassName("aboutMe");
+if (aboutMePrincipal.length > 0) {
+    aboutMePrincipal[0].innerHTML = "😐 About Me";
+}
+let aboutMeFact1 = document.getElementsByClassName("aboutMeFact1");
+if (aboutMeFact1.length > 0) {
+    aboutMeFact1[0].innerHTML = "🔭 Desvendando O Universo Da Programação";
+}
+let aboutMeFact2 = document.getElementsByClassName("aboutMeFact2");
+if (aboutMeFact2.length > 0) {
+    aboutMeFact2[0].innerHTML = "🎓 Estudante Front e Back End";
+}
+let aboutMeFact3 = document.getElementsByClassName("aboutMeFact3");
+if (aboutMeFact3.length > 0) {
+    aboutMeFact3[0].innerHTML = "🎸Guitarrista No Tempo Livre ";
+}
+let aboutMeFact4 = document.getElementsByClassName("aboutMeFact4");
+if (aboutMeFact4.length > 0) {
+    aboutMeFact4[0].innerHTML = "🚀Buscando Oportunidades De Trabalho ";
+}
